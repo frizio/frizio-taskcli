cc485bfcc97e1e858268478bbc3643251872ed30
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-01-19 11:27:39 +0100

    Command with args and action

--- src/commands.js
+++ src/commands.js
@@ -2,4 +2,11 @@ const { program } = require("commander");
 
 program.version("1.0.0").description("Task Management CLI");
 
+program
+  .command("save")
+  .action(() => {
+    console.log('Save action...');
+  })
+;
+
 program.parse(process.argv);
